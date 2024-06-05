f6d4a0724a3ca84ec7413e92976f0e956942c129
andrekumagae <andre.kumagae@hotmail.com>
2024-06-05 08:44:12 -0300

    adiciona projeto cafeteria

---
+++ dio/02-vivo-python-ai/01.ambiente-de-desenvolvimento-e-primeiros-passos-com-python/primeiro_programa.py
@@ -0,0 +1,4 @@
+print('Hello World')
+titulo = 'Ambiente de Desenvolvimento e Primeiros Passos com Python'
+titulo = titulo.replace(" ","-").lower()
+print(titulo)
---
+++ dio/02-vivo-python-ai/02-modo-interativo/notas.py
@@ -0,0 +1,8 @@
+# podemos udar o comando 'python' para roda-lo 
+# diretamente pelo terminal ou python -i arquivo.py 
+# para abrir o arquivo
+
+# mostrar metodos e objetos
+dir()
+# documentacao offline
+help()
