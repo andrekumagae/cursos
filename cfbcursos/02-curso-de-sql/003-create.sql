8223f8365abeeeb36ed279abfa3a91d20b38d4df
andrekumagae <andre.kumagae@hotmail.com>
2024-10-08 09:47:03 -0300

    organizacao de pastas

---
+++ cfbcursos/02-curso-de-sql/003-create.sql
@@ -0,0 +1,6 @@
+CREATE table cliente(
+	i_cliente_cliente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	s_nome_cliente VARCHAR(50) NOT NULL,
+	s_cpf_cliente VARCHAR(50) NOT NULL,
+	d_nasc_cliente DATETIME
+);
