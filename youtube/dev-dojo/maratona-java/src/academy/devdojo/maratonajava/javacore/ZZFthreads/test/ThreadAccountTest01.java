a945750010632b38d2601a5d6a5dddb3cea4cbe2
andre-kumagae <andre.kumagae@hotmail.com>
2025-02-18 22:49:11 -0300

    aula 225

--- youtube/dev-dojo/maratona-java/src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
+++ youtube/dev-dojo/maratona-java/src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
@@ -4,14 +4,13 @@ import academy.devdojo.maratonajava.javacore.ZZFthreads.dominio.Account;
 
 // implementaca da interface para utilizacao do metodo run()
 public class ThreadAccountTest01 implements Runnable {
-    private Account account = new Account();
+//    defina o atributo como final se for utilizar o synchronized dentro de um bloco
+    private final Account account = new Account();
 
     public static void main(String[] args) {
         ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
         Thread t1 = new Thread(threadAccountTest01, "Hestia");
         Thread t2 = new Thread(threadAccountTest01, "Bell Cranel");
-//        dando prioridade para forcar as 2 threads acessarem o saque concorrentemente
-        t1.setPriority(Thread.MAX_PRIORITY);
         t1.start();
         t2.start();
     }
@@ -29,13 +28,25 @@ public class ThreadAccountTest01 implements Runnable {
     }
 
 //    perceba que apesar de mesmo nome, este metodo apenas tem o if else para liberar o saque
+//    a palavra synchronized na assinatura do metodo barra o paralelismo entre as threads criando um lock
     private void withdrawal(int amount) {
-        if (account.getBalance() >= amount) {
-            System.out.println(getThreadName() + " está indo sacar dinheiro.");
-            account.withdrawal(amount);
-            System.out.println(getThreadName() + " completou o saque. Valor atual da conta: " + account.getBalance());
-        } else {
-            System.out.println("Sem dinheiro para "+ getThreadName() +" efetuar o saque "+account.getBalance());
+        // podemos utilizar o synchronized dentrod e um bloco do metodo
+        // para evitar problemas de uso de outro objeto, deixe-o como final
+        System.out.println(getThreadName() + " ### fora do bloco synchronized: ");
+        synchronized (account) {
+            System.out.println(getThreadName() + " *** dentro do bloco synchronized: ");
+            if (account.getBalance() >= amount) {
+                System.out.println(getThreadName() + " está indo sacar dinheiro.");
+                account.withdrawal(amount);
+//                try {
+//                    Thread.sleep(10000);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+                System.out.println(getThreadName() + " completou o saque. Valor atual da conta: " + account.getBalance());
+            } else {
+                System.out.println("Sem dinheiro para "+ getThreadName() +" efetuar o saque "+account.getBalance());
+            }
         }
     }
 
